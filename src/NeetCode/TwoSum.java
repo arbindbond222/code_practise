031e4e4daf80cf69b2474b768a5b5c340bc74c62
Arvind Gupta <radhagovinda008@gmail.com>
2024-05-05 13:55:11 +0530

    two sum with pair array class

--- src/NeetCode/TwoSum.java
+++ src/NeetCode/TwoSum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,15 +10,60 @@ import java.util.Map;
  * If it is, it prints the indices of the complement and the current number.
  * If not, it adds the current number and its index to the HashMap.
  *
- * @param nums the input array of integers
- * @param target the target sum value
  */
 public class TwoSum {
 
-    public static void main(String[] args){
+    static class PairArray implements Comparable<PairArray> {
+        int index;
+        int value;
+
+        PairArray(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(PairArray o) {
+            return this.value - o.value;
+        }
+    }
+
+    public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-        findSumPairUsingHashMap(nums, target);
+//        findSumPairUsingHashMap(nums, target);
+        findSumPairUsingPairArray(nums, target);
+    }
+
+    private static void findSumPairUsingPairArray(int[] nums, int target) {
+        //Here we are creating a new class called as PairClass this is our self made class
+        //we will also have comparator
+        PairArray[] pairArrays = new PairArray[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            pairArrays[i] = new PairArray(i, nums[i]);
+        }
+        Arrays.sort(pairArrays);
+        int[] res = new int[2];
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            int sum = pairArrays[left].value + pairArrays[right].value;
+            if(sum == target) {
+                res[0] = pairArrays[left].index;
+                res[1] = pairArrays[right].index;
+                break;
+            } else if(sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        //show result
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+
+
     }
 
     /**
@@ -27,7 +73,7 @@ public class TwoSum {
      * If it is, it prints the indices of the complement and the current number.
      * If not, it adds the current number and its index to the HashMap.
      *
-     * @param nums the input array of integers
+     * @param nums   the input array of integers
      * @param target the target sum value
      */
     private static void findSumPairUsingHashMap(int[] nums, int target) {
